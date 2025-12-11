876f214ca304dfa9277747dd70585b035b86f6b9
gn4ik <gnitienko220304@yandex.ru>
2025-12-11 22:06:21 +0300

    ...

--- .eslintrc.js
+++ .eslintrc.js
@@ -29,7 +29,14 @@ module.exports = {
     '@typescript-eslint/no-unused-vars': 'off',
     'no-undef': 'off',
     'react/react-in-jsx-scope': 'off',
-    'react/prop-types': 'off'
+    'react/prop-types': 'off',
+    '@typescript-eslint/no-explicit-any': 'warn',
+    'react/no-unknown-property': ['error', { ignore: ['stroke-width'] }],
+    'react-hooks/exhaustive-deps': 'warn',
+    'react-hooks/rules-of-hooks': 'error',
+    'prefer-const': 'error',
+    '@typescript-eslint/no-var-requires': 'off',
+    'react/no-unescaped-entities': 'off'
   },
   settings: {
     react: {
